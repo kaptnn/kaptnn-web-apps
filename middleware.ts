6081fb9cfac7251b301cbada201dbd151039ea39
Elsam Rafi Saputra <134671598+elsamrafisptr@users.noreply.github.com>
2025-04-24 11:22:02 +0700

    fix(middleware): fix middleware

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,33 @@
+import { UserApi } from "@/utils/axios/api-service";
+import { getCookie } from "@/utils/axios/utils";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+export const runtime = "edge";
+
+const AUTH_REQUIRED = ["/dashboard"];
+
+const ADMIN_ONLY = ["/dashboard/company", "/dashboard/category"];
+
 export async function middleware(request: NextRequest) {
-  console.log("Middleware running");
-  const { pathname } = request.nextUrl;
+  const { nextUrl } = request;
+  const { pathname } = nextUrl;
 
-  if (pathname.startsWith("/dashboard")) {
-    const access_token = request.cookies.get("access_token")?.value;
-    console.log("Token found:", access_token);
+  const token = await getCookie("access_token");
+  const currentUser = await UserApi.getCurrentUser(token);
+  const isAdmin = currentUser.profile.role === "admin";
 
-    if (!access_token) {
-      console.log("Redirecting to /login");
-      return NextResponse.redirect(new URL("/login", request.url));
+  if (AUTH_REQUIRED.includes(pathname)) {
+    if (!token) {
+      const loginUrl = nextUrl.clone();
+      loginUrl.pathname = "/login";
+      return NextResponse.redirect(loginUrl);
+    }
+
+    if (ADMIN_ONLY.includes(pathname) && !isAdmin) {
+      const homeUrl = nextUrl.clone();
+      homeUrl.pathname = "/dashboard";
+      return NextResponse.redirect(homeUrl);
     }
   }
 
